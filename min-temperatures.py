df5b653300c62ef34c4748e18a07460905d34380
Krzysztof Bober <krzysztof.bober@getbase.com>
2016-10-03 10:09:38 +0200

    Not convert it to Fahrenheit

--- min-temperatures.py
+++ min-temperatures.py
@@ -7,13 +7,12 @@ def parseLine(line):
     fields = line.split(',')
     stationID = fields[0]
     entryType = fields[2]
-    temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0
+    temperature = float(fields[3]) * 0.1
     return (stationID, entryType, temperature)
 
 lines = sc.textFile("1800.csv")
 parsedLines = lines.map(parseLine)
 minTemps = parsedLines.filter(lambda x: "TMIN" in x[1])
-
 stationTemps = minTemps.map(lambda x: (x[0], x[2]))
 
 # reduce by key - iterate by all enries and compare one(x) to the next one(y)
@@ -22,4 +21,4 @@ minTemps = stationTemps.reduceByKey(lambda x, y: min(x,y))
 results = minTemps.collect();
 
 for result in results:
-    print(result[0] + "\t{:.2f}".format(result[1] * (-17.2222)))
+    print(result[0] + "\t{:.2f}C".format(result[1]))
